861787799a7b5b7da4c559051ae0109d75d60e09
! レム <80942588+rem-9999@users.noreply.github.com>
2021-10-25 17:11:34 +0900

    Update embed.js

--- Codes/embed.js
+++ Codes/embed.js
@@ -8,9 +8,13 @@ module.exports = {
 		const args = message.content.slice(prefix.length).trim().split(/ +/);
 		let title = args.slice(1).join(" ").split("/")[0]
 		let des = message.content.split("/")[1]
-		let img = message.content.match(/\bhttps?:\/\/\S+/gi)[0]
+		let array = message.content.match(/\bhttps?:\/\/\S+/gi)[0]
 		if (!title) return message.reply("사용법 : !임베드 제목/설명/이미지(필수X)")
 		if (!des) return message.reply("사용법 : !임베드 제목/설명/이미지(필수X)")
+		        let img
+        if (array) {
+            img = array[0]
+        }
 		let embed = new MessageEmbed()
 			.setTitle(title)
 			.setDescription(des)
